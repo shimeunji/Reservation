10e07d9dae9889268b65ccfa96d480035ea73048
Eunji Shim <eunji991222@gmail.com>
2016-05-10 19:32:10 +0900

    #2 First Event Programming

--- app/src/main/java/kr/hs/emirim/kokopink1/reservation/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/kokopink1/reservation/MainActivity.java
@@ -2,12 +2,39 @@ package kr.hs.emirim.kokopink1.reservation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Chronometer;
+import android.widget.*;
+
 
 public class MainActivity extends AppCompatActivity {
+    Chronometer chrono;
+    Button butStart,butDone;
+    RadioButton radioDate,radioTime;
+    CalendarView calView;
+    TimePicker timeP;
+    TextView textResult;
+    // 참조 변수 선언
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //임플레이션 : 클래스를 패키지에서 찾고 주기억장치에 올려주는것
+        //레이아웃 화면 뜸!
+
+        chrono=(Chronometer)findViewById(R.id.chrono);
+        // findViewById 반환형 : View -> 강제 형 변환!
+        butStart=(Button)findViewById(R.id.but_start);
+        butDone=(Button)findViewById(R.id.but_done);
+        radioDate=(RadioButton)findViewById(R.id.radio_date);
+        radioTime=(RadioButton)findViewById(R.id.radio_time);
+        calView=(CalendarView)findViewById(R.id.calender);
+        timeP=(TimePicker)findViewById(R.id.time_pick);
+        textResult=(TextView)findViewById(R.id.text_result);
+
+        timeP.setVisibility(View.INVISIBLE); //보이지 않게
+        calView.setVisibility(View.INVISIBLE);
+
     }
 }
+
