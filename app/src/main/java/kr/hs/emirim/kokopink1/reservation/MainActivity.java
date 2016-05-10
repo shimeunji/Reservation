0893c1b81218153e12b24f21d0e3a0d01bfc1725
Eunji Shim <eunji991222@gmail.com>
2016-05-10 20:58:03 +0900

    #3 Second Event Programming

--- app/src/main/java/kr/hs/emirim/kokopink1/reservation/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/kokopink1/reservation/MainActivity.java
@@ -1,10 +1,14 @@
 package kr.hs.emirim.kokopink1.reservation;
 
+import android.graphics.Color;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Chronometer;
 import android.widget.*;
+
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -51,7 +55,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        butStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chrono.setBase(SystemClock.elapsedRealtime()); //기본적으로 시스템의 시간 사용할 때 명시
+                chrono.start(); //타이머 시간 설정!
+                chrono.setTextColor(Color.RED);
+            }
+        });
 
+        butDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chrono.stop();
+                chrono.setTextColor(Color.BLUE);
+                Calendar cal=Calendar.getInstance(); //Calendar 추상클래스의 겟인스턴스를 이용해 값구하기!@!@
+                cal.setTimeInMillis(calView.getDate());
+                int year=cal.get(Calendar.YEAR); //년도 반환
+                int month=cal.get(Calendar.MONTH)+1;
+                int date=cal.get(Calendar.DATE);
+
+                //int hour=timeP.getHour();
+                //int minute=timeP.getMinute();
+
+                String dateAndTime=year+"년 "+month+"분 "+date+"일 ";
+                textResult.setText(dateAndTime);
+
+            }
+        });
 
     }
 }
